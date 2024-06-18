70e0480efeabb557a943300e7b99e74b0fb9d119
Gustavo Duarte Ferreira g <gustavo64duarte@gmail.com>
2024-06-18 18:30:35 -0300

    Adicionar arquivos de projeto.

---
+++ DropShipping/Models/ProductModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DropShipping.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Descrição { get; set; }
+        public float Preço { get; set; }
+
+    }
+}
