2213bf35ca34472b19c3097f75a5b9645945d4a3
Gustavo Duarte Ferreira g <gustavo64duarte@gmail.com>
2024-06-25 10:59:28 -0300

    dark theme data-table

--- DropShipping/wwwroot/js/site.js
+++ DropShipping/wwwroot/js/site.js
@@ -4,6 +4,33 @@
 // Write your JavaScript code.
 
 $(document).ready(function () {
-    $('#tprodutos').DataTable();
+    $('#tprodutos').DataTable({
+        "ordering": true,
+        "paging": true,
+        "searching": true,
+        "oLanguage": {
+            "sEmptyTable": "Nenhum registro encontrado na tabela",
+            "sInfo": "Mostrar _START_ até _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrar 0 até 0 de 0 Registros",
+            "sInfoFiltered": "(Filtrar de _MAX_ total registros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "Mostrar _MENU_ registros por pagina",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Pesquisar",
+            "oPaginate": {
+                "sNext": "Proximo",
+                "sPrevious": "Anterior",
+                "sFirst": "Primeiro",
+                "sLast": "Ultimo"
+            },
+            "oAria": {
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            }
+        }
+    });
 });
 
